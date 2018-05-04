c68dc7c74b4251c4f7831e29f45ba176b00d44be
bzhang70 <bzhang70@illinois.edu>
2018-05-04 16:45:27 -0500

    add test script

--- dataset_gen.py
+++ dataset_gen.py
@@ -19,6 +19,25 @@ def valid_reprocess():
             if os.path.isdir(path + str(images[int(idx)])) is False: os.makedirs(path + str(images[int(idx)]), mode=0o777)
             cv2.imwrite(path + str(images[int(idx)]) + '/' + d, img)
 
+def test_reprocess():
+	images = {}
+        path = 'dataset/test/'
+        with open('imaterialist-challenge-furniture-2018/test.json') as json_data:
+            d = json.load(json_data)
+            labels = d['annotations']
+
+        for l in labels:
+            images[l['image_id']] = l['label_id']
+
+        train_data = os.listdir("imaterialist-challenge-furniture-2018/test_data")
+        for d in train_data:
+            if d=='.DS_Store': continue
+            idx = d.split('.')[0]
+            img = cv2.imread("imaterialist-challenge-furniture-2018/test_data/"+d)
+            if img is None:continue
+            if os.path.isdir(path + str(images[int(idx)])) is False: os.makedirs(path + str(images[int(idx)]), mode=0o777)
+            cv2.imwrite(path + str(images[int(idx)]) + '/' + d, img)
+
 def train_repreprocess():
         images = {}
         path = 'dataset/train/'
--- download_test.py
+++ download_test.py
@@ -5,17 +5,17 @@ import shutil
 
 os.chdir('imaterialist-challenge-furniture-2018/')
 
-valid_date = json.load(open('validation.json'))
+valid_date = json.load(open('test.json'))
 
 images_arr = valid_date['images']
 annotations_arr = valid_date['annotations']
-os.makedirs('valid_date')
+os.makedirs('test_date')
 for img in images_arr:
     if img['image_id'] % 1000 == 0:
-        print("Finished {} out of 194,828".format(img['image_id']))
+        print("Finished {} out of 12,000".format(img['image_id']))
     try:
         r = requests.get(img['url'][0],timeout=10)
-        folder_path = 'valid_data/'
+        folder_path = 'test_data/'
         with open(folder_path+str(img['image_id'])+'.jpg','wb') as f:
             f.write(r.content)
         f.close()
