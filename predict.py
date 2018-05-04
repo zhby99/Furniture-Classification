baa8c6053ae3840d24961e618f2cc98a93a72453
bzhang70 <bzhang70@illinois.edu>
2018-05-04 17:53:03 -0500

    add test script

--- predict.py
+++ predict.py
@@ -13,8 +13,22 @@ import time
 import os
 import copy
 
+import torchvision.transforms.functional as F
+
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+
+class HorizontalFlip(object):
+    """Horizontally flip the given PIL Image."""
+
+    def __call__(self, img):
+        """
+        Args:
+            img (PIL Image): Image to be flipped.
+        Returns:
+            PIL Image: Flipped image.
+        """
+        return F.hflip(img)
 
 def get_model():
     print('[+] loading model... ', end='', flush=True)
@@ -25,26 +39,71 @@ def get_model():
     return model
 
 
-data_transform = transforms.Compose([
+data_transforms = []
+
+data_transforms.append(transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
+]))
+data_transforms.append(transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    HorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+]))
 
 
 data_dir = 'dataset'
-image_dataset = datasets.ImageFolder(os.path.join(data_dir, 'test'), data_transform)
-dataloader = torch.utils.data.DataLoader(image_datasets['test'], batch_size=16,
-                                             shuffle=True, num_workers=1)
-dataset_size= len(image_dataset)
-# class_names = image_datasets['train'].classes
+data_loaders = []
+for transform in data_transforms:
+    image_dataset = datasets.ImageFolder(os.path.join(data_dir, 'test'), transform)
+    data_loader = torch.utils.data.DataLoader(image_dataset, batch_size=16, shuffle=False, num_workers=1)
+    data_loaders.append(data_loader)
+
 
 use_gpu = torch.cuda.is_available()
 
-def test_model(model):
-    model = get_model()
-    model.load_state_dict(torch.load('best_weight.pth'))
+
+def predict(model, dataloader):
+    all_outputs = []
     model.eval()
-    for data in dataloader:
-        
+
+    pbar = tqdm(dataloader, total=len(dataloader))
+    for inputs, labels in pbar:
+        inputs = Variable(inputs, volatile=True)
+        if use_gpu:
+            inputs = inputs.cuda()
+
+        outputs = model(inputs)
+        all_outputs.append(outputs.data.cpu())
+
+    all_outputs = torch.cat(all_outputs)
+    if use_gpu:
+        all_outputs = all_outputs.cuda()
+
+    return all_outputs
+
+def safe_stack_2array(a, b, dim=0):
+    if a is None:
+        return b
+    return torch.stack((a, b), dim=dim)
+
+
+def test_model(model):
+    prediction = None
+    for dataloader in dataloaders:
+        px = predict(model, dataloader)
+        prediction = safe_stack_2array(prediction, px, dim=-1)
+    return prediction
+
+model = get_model()
+model.load_state_dict(torch.load('best_weight.pth'))
+model.eval()
+prediction = test_model(model)
+data = {
+    'prediction': prediction.cpu(),
+}
+torch.save(data, 'test_prediction.pth')
