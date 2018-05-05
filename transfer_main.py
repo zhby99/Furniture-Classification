072cfd0959faa7b1b5cee6c3e8a742fa928f1ab3
bzhang70 <bzhang70@illinois.edu>
2018-05-05 14:45:38 -0500

    train setting

--- transfer_main.py
+++ transfer_main.py
@@ -13,17 +13,37 @@ import time
 import os
 import copy
 
+import torchvision.transforms.functional as F
+
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+
+class HorizontalFlip(object):
+    """Horizontally flip the given PIL Image."""
+
+    def __call__(self, img):
+        """
+        Args:
+            img (PIL Image): Image to be flipped.
+        Returns:
+            PIL Image: Flipped image.
+        """
+        return F.hflip(img)
 
 # Data augmentation and normalization for training
 # Just normalization for validation
 IMAGE_SIZE = 224
 data_transforms = {
     'train': transforms.Compose([
-        transforms.Resize((IMAGE_SIZE + 20, IMAGE_SIZE + 20)),
-        transforms.RandomCrop((IMAGE_SIZE, IMAGE_SIZE)),
-        transforms.RandomHorizontalFlip(),
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'train_flip': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        HorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
@@ -33,16 +53,23 @@ data_transforms = {
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
+    'val_flip': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        HorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
 }
 
 data_dir = 'dataset'
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
                                           data_transforms[x])
-                  for x in ['train', 'val']}
+                  for x in ['train', 'train_flip', 'val', 'val_flip']}
 dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=16,
                                              shuffle=True, num_workers=8)
-              for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+              for x in ['train', 'train_flip', 'val', 'val_flip']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'train_flip', 'val', 'val_flip']}
 class_names = image_datasets['train'].classes
 
 use_gpu = torch.cuda.is_available()
@@ -61,12 +88,12 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
         print('-' * 10)
 
         # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                scheduler.step()
-                model.train()  # Set model to training mode
-            else:
-                model.eval()  # Set model to evaluate mode
+        for phase in ['train','train_flip', 'val', 'val_flip']:
+            # if phase == 'train':
+            scheduler.step()
+            model.train()  # Set model to training mode
+            # else:
+            #     model.eval()  # Set model to evaluate mode
 
             running_loss = 0.0
             running_corrects = 0
@@ -100,18 +127,17 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
 
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
+            # # deep copy the model
+            # if phase == 'val' and epoch_acc > best_acc:
+            #     best_acc = epoch_acc
+            #     best_model_wts = copy.deepcopy(model.state_dict())
 
-        print()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
     print('Best val Acc: {:4f}'.format(best_acc))
-
+    best_model_wts = copy.deepcopy(model.state_dict())
     # load best model weights
     model.load_state_dict(best_model_wts)
     torch.save(best_model_wts, 'best_weight.pth')
@@ -135,6 +161,6 @@ criterion = nn.CrossEntropyLoss()
 optimizer_conv = optim.SGD(model_conv.classifier.parameters(), lr=0.001, momentum=0.9)
 
 # Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=5, gamma=0.1)
 
 model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=10)
